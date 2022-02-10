7910e3f81edae7c25df625f86c61a4dd38d6175d
Oyindamola Peterson <oyindamolapeterson@Oyindamolas-MacBook-Air.local>
2022-02-09 23:42:20 -0500

    update files

--- routes/ApiRoutes.js
+++ routes/ApiRoutes.js
@@ -1,71 +1,10 @@
-// const notes = require("../db/db.json");
-// const fs = require("fs");
-
-// const { v4: uuidv4 } = require("uuid");
-// const readDb = require("../db/db.json");
-// const writeDb = require("../db/db.json");
-
-// // Routes
-// // root route returns index.html
-// module.exports = function (app) {
-//   // routes to return db.json file constaining notes
-//   app.get("/api/notes", function (req, res) {
-//     console.log("from server");
-//     const db = readDb;
-//     res.json(db);
-//   });
-
-//   // routes to return db.json file constaining notes
-//   app.post("/api/notes", function (req, res) {
-//     // generate unique id
-//     const uuid = uuidv4();
-//     // receive the new note from the webserver
-//     const newNote = req.body;
-//     //set uuid value
-//     newNote.id = uuid;
-
-//     // set variable to gather all data and seed it with data in the file
-//     let db = readDb;
-
-//     // append the newNote data to the variable with the file data
-//     db.push(newNote);
-
-//     // overwrite file
-//     // calls function exported from file '../db/writeDbFile';
-//     writeDb(db);
-
-//     //return the data as JSON in the response
-//     res.json(db);
-//   });
-
-//   // route to delete note (specific by id)
-//   app.delete("/api/notes/:id", function (req, res) {
-//     // read in the file containing the notes db
-//     const db = readDb;
-//     // get value of id from params
-//     const deleteId = req.params.id;
-
-//     for (let i = 0; i < db.length; i += 1) {
-//       if (db[i].id === deleteId) {
-//         // removes the i element from array
-//         db.splice(i, 1);
-//         // no need to contiue looking for element to delete
-//         break;
-//       }
-//     }
-
-//     //return the data as JSON in the response
-//     res.json(db);
-//   });
-// };
-
 const router = require("express").Router();
 
-const helpers = require("../helpers");
+const controllers = require("../controllers");
 
 // GET request
 router.get("/notes", function (req, res) {
-  helpers
+  controllers
     .retrieveNotes()
     .then((notes) => res.json(notes))
     .catch((err) => res.status(500).json(err));
@@ -73,7 +12,7 @@ router.get("/notes", function (req, res) {
 
 // POST request
 router.post("/notes", (req, res) => {
-  helpers
+  controllers
     .addNote(req.body)
     .then((note) => res.json(note))
     .catch((err) => res.status(500).json(err));
@@ -81,7 +20,7 @@ router.post("/notes", (req, res) => {
 
 // Bonus - DELETE request
 router.delete("/notes/:id", function (req, res) {
-  helpers
+  controllers
     .deleteNote(req.params.id)
     .then(() => res.json({ ok: true }))
     .catch((err) => res.status(500).json(err));
--- routes/pagenote.js
+++ routes/pagenote.js
@@ -1,13 +1,13 @@
 var path = require("path");
-const app = require("express").Router();
+const router = require("express").Router();
 // Route will return the notes HTMl file from the public folder
 
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(`${__dirname}/../public`, "notes.html"));
-});
+router.get("/notes", (req, res) =>
+  res.sendFile(path.join(`${__dirname}/../public`, "notes.html"))
+);
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(`${__dirname}/../public`, "index.html"));
-});
+router.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/index.html"))
+);
 
-module.exports = app;
+module.exports = router;
